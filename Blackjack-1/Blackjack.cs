7168023a9d185eb6d4b414f0f6e76166d4e82046
kim sunjin <rlatjswls14@gmail.com>
2026-03-03 00:08:18 +0900

    제출

---
+++ Blackjack-1/Blackjack.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+internal class Blackjack
+{
+    public readonly string[] CardNumbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    public readonly string[] CardSuits = { "♠", "♡", "◇", "♣" };
+
+    public int[] CurrDeck;
+
+    public int Counter;
+
+    public int CurrentCoin; //플레이중 변경되는 코인
+
+    public readonly int MaxCoin=1000; //시작시 가지고있는 코인
+
+    public int InputCoin;//입력받은 코인값 저장
+  
+
+
+    public Blackjack()
+    {
+        CurrentCoin = MaxCoin; //코인을 1000으로 초기화
+        NewDeck();
+    }
+      
+
+
+    private void NewDeck()
+    {
+
+        CurrDeck = new int[52];
+
+        Counter = 0;
+
+        Console.WriteLine("카드를 섞는 중...");
+
+        for (int i = 0; i < CurrDeck.Length; i++)
+        {
+            CurrDeck[i] = i;
+        }
+
+        for (int i = CurrDeck.Length - 1; i > 0; i--)
+        {
+            Random random = new Random();
+            int x = random.Next(0, i + 1);
+
+            int temp = CurrDeck[i];
+            CurrDeck[i] = CurrDeck[x];
+            CurrDeck[x] = temp;
+        }
+
+    }
+    public int Draw()
+    {
+        if (Counter > CurrDeck.Length - 1)
+        {
+            Console.WriteLine("새로운 덱을 열었습니다");
+            NewDeck();
+        }
+       return CurrDeck[Counter++];
+    }
+
+
+    
+    public void ShowDealer(Dealer dealer)
+    {
+        string cardSuit;
+        string cardNumber;
+
+        Console.Write($"딜러의 패: ");
+        for (int i = 0; i < dealer.Counter; i++)
+        {
+            cardSuit = CardSuits[dealer.Hands[i] / 13];
+            cardNumber = CardNumbers[dealer.Hands[i] % 13];
+            if (!dealer.IsOpened && i == 0)
+            {
+                Console.Write($"[??] ");
+            }
+            else
+            {
+                Console.Write($"[{cardSuit} {cardNumber}] ");
+            }
+        }
+      
+        Console.WriteLine();
+        Console.Write($"딜러 점수: {(dealer.IsOpened ? dealer.Score : " ??")}");
+       
+
+    }
+
+    public void ShowPlayer(Player player)
+    {
+        string cardSuit;
+        string cardNumber;
+
+        Console.Write($"플레이어의 패: ");
+        for (int i = 0; i < player.Counter; i++)
+        {
+            cardSuit = CardSuits[player.Hands[i] / 13];
+            cardNumber = CardNumbers[player.Hands[i] % 13];
+            
+                Console.Write($"[{cardSuit} {cardNumber}] ");
+          
+        }
+        Console.WriteLine();
+        Console.WriteLine($"플레이어 점수: {player.Score}");
+
+    }
+
+
+    public void secretcard(Dealer dealer)//이게 새로만든 매소드 입니다.
+    { 
+        int i = 0;
+        string cardSuit;
+        string cardNumber;
+        cardSuit = CardSuits[dealer.Hands[i] / 13];
+        cardNumber = CardNumbers[dealer.Hands[i] % 13];
+        Console.WriteLine($"딜러의 숨겨진 카드: [{cardSuit} {cardNumber}]");
+    }
+
+
+    public void Betting() //배팅값을 사용자에게 받은후 검사
+    {
+        Console.WriteLine($"보유 칩: {CurrentCoin}");
+
+     
+        while(true)
+            {
+            Console.Write($"베팅 금액을 입력하세요: ");
+            string input = Console.ReadLine();
+            int.TryParse(input, out int inputcoin);
+
+            if (inputcoin > CurrentCoin)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"보유금액을 초과했습니다");
+                Console.WriteLine($"다시 입력해 주세요");
+                Console.WriteLine();
+            }
+            else if (inputcoin < 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"0이하의 금액을 배팅할수 없습니다");
+                Console.WriteLine($"다시 입력해 주세요");
+                Console.WriteLine();
+            }
+            else if (inputcoin <= CurrentCoin && inputcoin > 0)
+            {
+                Console.WriteLine();
+                InputCoin = inputcoin;
+                break;
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"배팅할 금액을 입력해 주세요");
+                Console.WriteLine();
+            }
+
+            }
+    }
+
+
+
+
+}
+
--- Blackjack-1/Program.cs
+++ Blackjack-1/Program.cs
@@ -1,3 +1,181 @@
 using System;
+using System.Net.Sockets;
 
-Console.WriteLine("Hello, Blackjack");
+
+Blackjack blackjack = new();//하나의 덱을 생성한고 게임이 끝날때까지 사용하기위해 사용
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("===블랙잭 게임===");
+    Console.WriteLine("카드를 섞는 중...");
+    Dealer dealer = new();
+    Player player = new();
+    Console.WriteLine("===초기패===");
+    blackjack.Betting();
+    dealer.AddHands(blackjack.Draw(), blackjack.Draw());
+    blackjack.ShowDealer(dealer);
+    Console.WriteLine();
+    Console.WriteLine();
+    player.AddHands(blackjack.Draw(), blackjack.Draw());
+    blackjack.ShowPlayer(player);
+    Console.WriteLine();
+
+    while (true)
+    {
+        Console.Write("H(Hit) 또는 S(Stand)를 선택하세요:");
+        string input = Console.ReadLine().ToUpper();
+        if (input == "H")
+        {
+            int card = blackjack.Draw();
+            player.AddHands(card);
+            Console.WriteLine();
+            Console.WriteLine($"플레이어가 카드를 받았습니다: [{blackjack.CardSuits[card / 13]} {blackjack.CardNumbers[card % 13]}]");
+
+            blackjack.ShowPlayer(player);
+            if (player.IsOver)
+            {
+                Console.WriteLine();
+                Console.WriteLine("21을 넘었습니다");
+                Console.WriteLine();
+
+                break;
+            }
+        }
+        else if (input == "S")
+        {
+            Console.WriteLine("플레이어가 Stand를 선택했습니다");
+            Console.WriteLine();
+            break;
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("다시 선택 해주세요");
+        }
+
+    }
+
+
+    if (player.IsOver) //플레이어 카드가 21을 넘으면 딜러차례없이 게임종료
+    {
+        GameResult();// 현재코인이 0보다 적으면 그대로 프로그램을 끝나게 만들었는데 모든정보를 리셋하고 코인을 1000 부터 다시 시작하는게 맞는걸까요?
+        if (blackjack.CurrentCoin<=0)
+        { 
+            return;
+        }
+    }
+    else
+    {
+        dealer.IsOpened = true; //딜러 true 만드는방법이 안떠올라서 그냥 입력해버렸습니다. 확인부탁드려요 
+        blackjack.secretcard(dealer); // 딜러의 시크릿 카드 오픈
+        blackjack.ShowDealer(dealer);
+        Console.WriteLine();
+
+        while (true)
+        {
+            if (dealer.Score < 17)
+            {
+                int card = blackjack.Draw();
+                dealer.AddHands(card);
+                Console.WriteLine();
+                Console.WriteLine($"딜러가 카드를 받았습니다: [{blackjack.CardSuits[card / 13]} {blackjack.CardNumbers[card % 13]}]");
+                blackjack.ShowDealer(dealer);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine();
+                break;
+
+            }
+        }
+        GameResult();
+        if (blackjack.CurrentCoin <= 0)
+        {
+            return;
+        }
+        Console.WriteLine();
+    }
+    for (; ; )
+    {
+        Console.WriteLine("새 게임을 하시겠습니까? (Y/N): ");
+        string input = Console.ReadLine().ToUpper();
+        if (input == "Y")
+        {
+            break;
+        }
+
+        else if (input == "N")
+        {
+            Console.WriteLine("게임을 종료합니다");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("다시 입력해 주세요");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    void GameResult()
+    {
+        Console.WriteLine("===게임 결과===");
+        Console.WriteLine();
+        Console.WriteLine($"플레이어: {player.Score}");
+        Console.WriteLine($"딜러: {dealer.Score}");
+        Console.WriteLine();
+        if (21 < player.Score)
+        {
+            Console.WriteLine("딜러의 승리");
+            blackjack.CurrentCoin -= blackjack.InputCoin;
+            Console.WriteLine($"딜러의 승리!(-{blackjack.InputCoin})");
+            Console.WriteLine($"보유칩:({blackjack.CurrentCoin})");
+            if (blackjack.CurrentCoin <= 0)//현재 가지고 있는 코인이 0이하일 경우 게임오버
+            {
+                Console.WriteLine($"보유한 코인이 없습니다");
+                Console.WriteLine($"게임오버");
+            }
+        }
+        else if (21 < dealer.Score)
+        {
+            Console.WriteLine("플레이어의 승리");
+            blackjack.CurrentCoin += blackjack.InputCoin;
+            Console.WriteLine($"플레이어의 승리!(+{blackjack.InputCoin})");
+            Console.WriteLine($"보유칩:({blackjack.CurrentCoin})");
+        }
+        else if (player.Score < dealer.Score)
+        {
+            Console.WriteLine("딜러의 승리");
+            blackjack.CurrentCoin -= blackjack.InputCoin;
+            Console.WriteLine($"딜러의 승리!(-{blackjack.InputCoin})");
+            Console.WriteLine($"보유칩:({blackjack.CurrentCoin})");
+            if (blackjack.CurrentCoin <= 0)
+            {
+                Console.WriteLine($"보유한 코인이 없습니다");
+                Console.WriteLine($"게임오버");
+            }
+        }          
+        else if (player.Score > dealer.Score)
+        {
+            Console.WriteLine("플레이어의 승리");
+            blackjack.CurrentCoin += blackjack.InputCoin;
+            Console.WriteLine($"플레이어의 승리!(+{blackjack.InputCoin})");
+            Console.WriteLine($"보유칩:({blackjack.CurrentCoin})");
+        }
+        else
+        {
+            Console.WriteLine("무승부");
+        }
+    }
+
+    
+}
